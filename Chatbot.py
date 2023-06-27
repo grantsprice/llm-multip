d05471983fb41ba91ff4a73f6551cba3e436db2a
grant price <gprice@usc.edu>
2023-06-27 16:36:43 -0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,11 +2,6 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
-with st.sidebar:
-    #openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
 st.title("💬 Multip GPT")
 openai_api_key = st.secrets["chatbot_api_key"]
 if "messages" not in st.session_state:
